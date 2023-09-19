2bd7d70165b89fc2bb123a13a5e5c39b6e15fd1e
nathctout ytb <nathanpa.etude@gmail.com>
2023-09-19 16:38:46 +0200

    admin

--- Symfony/src/Security/LoginAuthenticator.php
+++ Symfony/src/Security/LoginAuthenticator.php
@@ -49,8 +49,9 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //return new RedirectResponse($this->urlGenerator->generate('some_route'));
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('admin'));
     }
 
     protected function getLoginUrl(Request $request): string
