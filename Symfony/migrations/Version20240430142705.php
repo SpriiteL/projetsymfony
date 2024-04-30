385c357424d40fd7cc0da5f8d8c3086b36dfa1ea
nathctout ytb <nathanpa.etude@gmail.com>
2024-04-30 16:27:40 +0200

    migration

--- Symfony/migrations/Version20240430142705.php
+++ Symfony/migrations/Version20240430142705.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240430142441 extends AbstractMigration
+final class Version20240430142705 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,17 +20,12 @@ final class Version20240430142441 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE category ADD product_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE category ADD CONSTRAINT FK_64C19C14584665A FOREIGN KEY (product_id) REFERENCES product (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_64C19C14584665A ON category (product_id)');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE category DROP CONSTRAINT FK_64C19C14584665A');
-        $this->addSql('DROP INDEX UNIQ_64C19C14584665A');
-        $this->addSql('ALTER TABLE category DROP product_id');
     }
 }
